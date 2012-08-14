133d29ebf5ef9e6cc7ccc8f852f764b2a86e7662
Sebastien PRUNIER <sebastien.prunier@gmail.com>
2012-08-14 16:36:59 +0200

    init maven project

---
+++ src/main/java/org/bonitasoft/demo/callforpaper/model/CFP.java
@@ -0,0 +1,35 @@
+package org.bonitasoft.demo.callforpaper.model;
+
+public class CFP {
+
+	private Long id;
+
+	private String speaker;
+
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSpeaker() {
+		return speaker;
+	}
+
+	public void setSpeaker(String speaker) {
+		this.speaker = speaker;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
