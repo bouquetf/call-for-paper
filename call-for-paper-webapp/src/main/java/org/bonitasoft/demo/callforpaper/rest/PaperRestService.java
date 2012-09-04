4aebeee004204af4a6b2715d00cb50192115e1a5
Sebastien PRUNIER <sebastien.prunier@gmail.com>
2012-09-04 10:46:09 +0200

    expose paper update

--- call-for-paper-webapp/src/main/java/org/bonitasoft/demo/callforpaper/rest/PaperRestService.java
+++ call-for-paper-webapp/src/main/java/org/bonitasoft/demo/callforpaper/rest/PaperRestService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -58,6 +59,14 @@ public class PaperRestService {
 		paperService.createPaper(paper);
 		URI uri = uriInfo.getAbsolutePathBuilder().path(paper.getId().toString()).build();
 		return Response.created(uri).build();
+	}
+	
+	@PUT
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response updatePaper(Paper paper) {
+		paperService.updatePaper(paper);
+		URI uri = uriInfo.getAbsolutePathBuilder().path(paper.getId().toString()).build();
+		return Response.ok(uri).build();
 	}
 
 	@DELETE
